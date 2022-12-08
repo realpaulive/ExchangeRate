621de04268f96f2fbc4cdd67a120b0850b0f83f7
realpaulive <ps.upstairs@gmail.com>
2022-12-09 01:13:25 +0300

    Initial Commit Views and ViewControllers was added

---
+++ ExchangeRate/Models/CurranciesData.swift
@@ -0,0 +1,42 @@
+//
+//  CurranciesData.swift
+//  ExchangeRate
+//
+//  Created by Paul Ive on 09.12.22.
+//
+
+import Foundation
+
+// MARK: - ExchageCurrancies
+struct ExchageCurrancies: Codable {
+    let date, previousDate: Date
+    let previousURL: String
+    let timestamp: Date
+    let valute: [String: Valute]
+
+    enum CodingKeys: String, CodingKey {
+        case date = "Date"
+        case previousDate = "PreviousDate"
+        case previousURL = "PreviousURL"
+        case timestamp = "Timestamp"
+        case valute = "Valute"
+    }
+}
+
+// MARK: - Valute
+struct Valute: Codable {
+    let id, numCode, charCode: String
+    let nominal: Int
+    let name: String
+    let value, previous: Double
+
+    enum CodingKeys: String, CodingKey {
+        case id = "ID"
+        case numCode = "NumCode"
+        case charCode = "CharCode"
+        case nominal = "Nominal"
+        case name = "Name"
+        case value = "Value"
+        case previous = "Previous"
+    }
+}
---
+++ ExchangeRate/ViewControllers/CurrenciesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  CurrenciesViewController.swift
+//  ExchangeRate
+//
+//  Created by Paul Ive on 09.12.22.
+//
+
+import UIKit
+
+class CurrenciesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
