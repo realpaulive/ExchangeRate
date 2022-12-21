4abcbcb70a49b68b2bc6025441d669c43d2c7c5d
realpaulive <realpaulive@gmail.com>
2022-12-21 19:40:03 +0300

    New networking file

--- ExchangeRate/Favorites/FavoritesViewController.swift
+++ ExchangeRate/Favorites/FavoritesViewController.swift
@@ -7,15 +7,44 @@
 
 import UIKit
 
+
+
 class FavoritesViewController: UIViewController {
     
+    @IBOutlet weak var tableView: UITableView!
+    
+    var favoritesArray = ["USD"]
+    
+    let updatedCurrencies = UpdateCurrencies.shared
  
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.reloadData()
        
     }
 
 
+}
+
+extension FavoritesViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return favoritesArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoriteCell") as! FavoriteViewCell
+        
+        let key = favoritesArray[indexPath.row]
+        let value = updatedCurrencies.valutes[key]
+        guard let values = value else { return cell }
+        cell.setUpFavoriteCell(valutes: values, key: key)
+        return cell
+    }
+    
+    
 }
 
 
---
+++ ExchangeRate/Favorites/View/FavoriteViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  FavoriteViewCell.swift
+//  ExchangeRate
+//
+//  Created by Paul Ive on 18.12.22.
+//
+
+import UIKit
+
+class FavoriteViewCell: UITableViewCell {
+
+    @IBOutlet weak var currencyImage: UIImageView!
+    @IBOutlet weak var currencyName: UILabel!
+    @IBOutlet weak var dailyChangeValue: UILabel!
+    @IBOutlet weak var dailyChangePercent: UILabel!
+    @IBOutlet weak var currencyKey: UILabel!
+    @IBOutlet weak var currencyValue: UILabel!
+    
+    func setUpFavoriteCell (valutes: Valutes, key: String) {
+        self.currencyKey?.text = key
+        self.currencyName?.text = { return valutes.nominalString + " " + valutes.name }()
+        self.currencyValue.text =  { return valutes.currencyValueString + " ₽" }()
+        self.dailyChangePercent.text = valutes.dailyChangePercentString
+        self.dailyChangePercent.textColor = UIColor(named: valutes.dailyChangeColor)
+    
+        self.dailyChangeValue.text = valutes.dailyChangeValueString 
+        self.dailyChangeValue.textColor = UIColor(named: valutes.dailyChangeColor)
+        self.currencyImage.image = UIImage(named: key)
+        self.currencyImage.layer.cornerRadius = self.currencyImage.frame.size.height / 4
+        self.currencyImage.layer.borderWidth = 0.2
+        self.currencyImage.layer.borderColor = CGColor(gray: 0.1, alpha: 1)
+    }
+}
